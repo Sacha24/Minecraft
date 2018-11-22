44c032571e92ae7779ef98086512a7161a311933
Sacha <sacha.auday@hotmail.fr>
2018-11-22 19:32:59 +0200

    code optimized

--- js/Minecraft2.js
+++ js/Minecraft2.js
@@ -1,136 +1,95 @@
 $(document).ready(function () {
 
 world = [
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
-    ["sky", "sky", "cloud", "cloud", "cloud", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
-    ["sky", "sky", "sky", "cloud", "cloud", "cloud", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "cloud", "sky", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "tree", "sky", "sky", "sky",],
-    ["sky", "sky", "sky", "sky", "sky", "sky", "rock", "rock", "sky", "tree", "sky", "sky", "rock",],
-    ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass",],
-    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
-    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
-    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
-    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",]
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "cloud", "cloud", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "cloud", "cloud", "cloud", "cloud", "cloud", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "cloud", "cloud", "sky", "sky", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "tree", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "rock", "rock", "sky", "tree", "sky", "sky", "rock", "sky", "sky", "sky",],
+    ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",]
 ];
 
 function generateWorld (world) {
-    for (var i = 0; i < world.length; i++) {
-        $("#world").append("<div class='row justify-content-center'>");
-        for (var j = 0; j < world[0].length; j++) {
-            $(".row:nth-child("+ i + ")").append("<div class='pixel " + world[i][j] + "'>");
-        }
-    }
+    var rows = "<div class='row justify-content-end'>";
+    world.forEach((w,i) => {  $("#world").append(rows), 
+        w.forEach((s, j)=>{$(".row:nth-child("+ i + ")").append("<div class='pixel " + world[i][j] + "'>")} )})
 }
 generateWorld(world);
 
-function pixelBorder(){
-    var square = event.currentTarget;
-    square.style.border = "1px solid white";
+function startGame(){
+    $("#world").css("display","block")
+    $("button").css("display", "none")
 }
-$(".pixel").on("mouseover", pixelBorder)
+$("#startGame").on("click", startGame)
 
-function removePixelBorder(){
-    var square = event.currentTarget;
-    square.style.border = "none";
+var toolSelected = ""  // get the tool I selected
+function pickTool(){
+    $(".tool").css("background-color", "black")
+    toolSelected = event.target;
+    toolSelected.style.backgroundColor = "blue"
+    toolSelected = event.target.classList[1];
 }
-$(".pixel").on("mouseout", removePixelBorder)
+$(".tool").on("click", pickTool)
 
 
-function shovel (){
-    var square = event.currentTarget;
-    square.classList.replace("grass", "sky")
-    square.classList.replace("dirt", "sky")
-}
-$(".tool.shovel").on("click", function (){
-    $(".pixel.dirt").on("click", getLastElement)
-    $(".pixel.grass").on("click", getLastElement)
-    $(".pixel.tree").off("click", getLastElement)
-    $(".pixel.leaf").off("click", getLastElement)
-    $(".tool.shovel").css("background-color","blue")
-    $(".tool.pickAxe").css("background-color","black")
-    $(".tool.axe").css("background-color","black")
-    $(".pixel.dirt").on("click", shovel)
-    $(".pixel.grass").on("click", shovel)
-    $(".pixel.rock").off("click", pickAxe)
-    $(".pixel.tree").off("click", Axe)
-    $(".pixel.leaf").off("click", Axe)
-})
-
-function pickAxe(){
-    var square = event.currentTarget;
-    square.classList.replace("rock", "sky")
-}
-$(".tool.pickAxe").on("click", function (){
-    $(".pixel.rock").on("click", getLastElement)
-    $(".pixel.dirt").off("click", getLastElement)
-    $(".pixel.grass").off("click", getLastElement)
-    $(".tool.shovel").css("background-color","black")
-    $(".tool.pickAxe").css("background-color","blue")
-    $(".tool.axe").css("background-color","black")
-    $(".pixel.rock").on("click", pickAxe)
-    $(".pixel.dirt").off("click", shovel)
-    $(".pixel.grass").off("click", shovel)
-    $(".pixel.tree").off("click", Axe)
-    $(".pixel.leaf").off("click", Axe)
-})
-
-function Axe(){
-    var square = event.currentTarget;
-    square.classList.replace("tree", "sky")
-    square.classList.replace("leaf", "sky")
-}
-$(".tool.axe").on("click", function (){
-    $(".pixel.tree").on("click", getLastElement)
-    $(".pixel.leaf").on("click", getLastElement)
-    $(".pixel.dirt").off("click", getLastElement)
-    $(".pixel.grass").off("click", getLastElement)
-    $(".pixel.rock").off("click", getLastElement)
-    $(".tool.axe").css("background-color","blue")
-    $(".tool.shovel").css("background-color","black")
-    $(".tool.pickAxe").css("background-color","black")
-    $(".pixel.tree").on("click", Axe)
-    $(".pixel.leaf").on("click", Axe)
-    $(".pixel.dirt").off("click", shovel)
-    $(".pixel.grass").off("click", shovel)
-    $(".pixel.rock").off("click", pickAxe)
-})
-
-var tile = [];
+var useTile = false;
+var tile = [];  // stock all the elements I took off
 function getLastElement() {
-    var square = event.currentTarget;
-    tile.push(square.className);
-    var last = tile[tile.length-1].replace("pixel ","");
-    $(".tile").removeClass().addClass("tile " + last)
+    if(!useTile){
+        var square = event.target;
+        tile.push(square.classList[1]);
+        last = square.classList[1];
+        // stock only the elements that I allow to take off according the tool I selected
+        if((toolSelected === "shovel" && last === "grass")   
+        ||(toolSelected === "shovel" && last === "dirt")){
+            $(".tile").removeClass().addClass("tile " + last)
+            square.classList.replace("grass", "sky");
+            square.classList.replace("dirt", "sky");
+        }
+        if(toolSelected === "pickAxe" && last === "rock"){
+            $(".tile").removeClass().addClass("tile " + last);
+            square.classList.replace("rock", "sky");
+        }
+        if((toolSelected === "axe" && last === "leaf")
+        ||(toolSelected === "axe" && last === "tree")){
+            $(".tile").removeClass().addClass("tile " + last);
+            square.classList.replace("tree", "sky");
+            square.classList.replace("leaf", "sky");
+        }
+    }
+    else{
+        $(".tool").css("background-color","black")
+        tile_type = $(".tile")[0].classList[1];
+        if(this.classList[1] === "sky"){
+            $(this).attr('class', 'pixel ' + tile_type);
+        }
+        else{
+            $(this).attr('class', this.classList);
+        }
+        useTile = false;
+    }
 }
+$(".pixel").on("click", getLastElement);
 
 function useLastElement() {
-    var square = event.currentTarget;
+    var square = event.target;
     var last = tile[tile.length-1].replace("pixel ","");
-    square.classList.replace("sky", last); 
+    square.classList.replace("sky", last);
     $(".tile."+last).removeClass(last)
     $(".pixel.sky").off("click", useLastElement)
 }
+
 $(".tile").on("click", function(){
-    $(".pixel.sky").on("click", useLastElement)
-    $(".pixel").off("click", getLastElement)
-    $(".pixel").off("click", shovel)
-    $(".pixel").off("click", Axe)
-    $(".pixel").off("click", pickAxe)
-    $(".tool.shovel").css("background-color","black")
-    $(".tool.axe").css("background-color","black")
-    $(".tool.pickAxe").css("background-color","black")
+    useTile = true;
 })
-
-
-
-
-
-
-
 });
